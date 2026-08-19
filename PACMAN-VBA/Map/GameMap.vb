392602a5590682925dbefc5245bcfeec2eb01603
F3stus-0 <ojedaceballosluisdavid@gmail.com>
2026-08-18 19:26:13 -0500

    Updated Gamemap
    This is the progress I made on school (almost nothing)

--- PACMAN-VBA/Map/GameMap.vb
+++ PACMAN-VBA/Map/GameMap.vb
@@ -1,14 +1,36 @@
 ﻿Public Class GameMap
-    'Dimensions
-    Public Const columns As Integer = 28
-    Public Const rows As Integer = 36
 
-    'Matrix for the Map
-    'At least for now 0 will represent a wall and 1 the floor
-    Public MazeMatrix(columns - 1, rows - 1) As Integer
+    Public Const Width As Integer = 28
+    Public Const Height As Integer = 31
+
+    Private MapMatrix(Width - 1, Height - 1) As Integer
 
     Public Sub New()
-        'LoadMap()
+
+        For Y As Integer = 0 To Height - 1
+
+            For X As Integer = 0 To Width - 1
+
+                MapMatrix(X, Y) = Mapdata.ClassicPacMan(Y, X)
+
+            Next
+
+        Next
+
     End Sub
+
+    Public Function IsWalkable(X As Integer, Y As Integer) As Boolean
+
+        If X < 0 OrElse X >= Width Then
+            Return False
+        End If
+
+        If Y < 0 OrElse Y >= Height Then
+            Return False
+        End If
+
+        Return MapMatrix(X, Y) = 1
+
+    End Function
 
 End Class
---
+++ PACMAN-VBA/Map/MapData.vb
@@ -0,0 +1,3 @@
+﻿Public Class MapData
+
+End Class
